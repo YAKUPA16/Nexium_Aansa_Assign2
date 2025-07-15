dd4f0ee339530e17b89b8715290318de4a604471
Aansa Younas <aansaunas@gmail.com>
2025-07-15 19:46:48 +0500

    Update layout.tsx

--- blogbite/app/layout.tsx
+++ blogbite/app/layout.tsx
@@ -1,22 +1,10 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "BlogBite | Blog Summarizer",
   description: "Summarize blogs the tasty way 🍪",
   icons: {
-    icon: "/favicon.jpg",
+    icon: "/favicon.jpg", // Make sure this file exists in the public folder
   },
 };
 
